5e3844b3c129fa209f5351ecefa0ad92399f62e7
G Nikhil <garudanikhil21@gmail.com>
2021-12-03 11:17:39 +0530

    .

--- election_site/election/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Post)
-admin.site.register(Voter)
-admin.site.register(Candidate)
-admin.site.register(Election_Coordinator)
-admin.site.register(Vote)
